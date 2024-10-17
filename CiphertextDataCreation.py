c333bccf870b91d7e7c0f58a189b4d1846e155c6
CharlieKlawitter <charlieklawitter@gmail.com>
2024-10-17 16:50:24 -0500

    Update CiphertextDataCreation.py

--- CiphertextDataCreation.py
+++ CiphertextDataCreation.py
@@ -1,34 +1,38 @@
 import random  as random 
+import pyquotegen
+
+# %%
 
 #Shift Cipher Encryption
-def shift_cipher_encrypt(plaintext, key): 
-    ciphertext = "" 
-    for char in plaintext:
-        if char.isupper(): 
-            ciphertext += chr((ord(char) + key - 65) % 26 + 65) 
-        elif char.islower():
-            ciphertext += chr((ord(char) + key - 97) % 26 + 97) 
-        else: 
-            ciphertext += char 
-        
+def shift(quote, key):
+    ciphertext = ""
+    for char in quote:
+        ciphertext += chr((ord(char) + key - 97) % 26 + 97)
+       
+    ciphertext = ciphertext.upper()
 
-    ciphertext = ciphertext.upper() 
-    ciphertext = ciphertext.replace(" ", "")  
-    return ciphertext 
-    
+    return ciphertext
+
 #Generate Encrypted Data
 
-def data(text, num_samples=1000):
+def data(quote, num_samples=1000):
     dataset = []
     for _ in range(num_samples):
-        key = random.randint(0, 25)
-        ciphertext = shift_cipher_encrypt(text, key)
-        dataset.append((ciphertext, key))
+        quote = pyquotegen.get_quote() #Get a new quote for every iteration
+        quote = quote.replace("," , "").replace('.' , "").replace("'" , "").replace(" ", "") #Clean up quote 
+        quote = quote.lower() #
+        key = random.randint(1, 25)
+        ciphertext = shift(quote, key)
+        dataset.append((quote, ciphertext, key))
     return dataset
 
-#Dataset Generation 
 
-plaintext = "This is an example of shift cipher encryption to generate a data set"
-dataset = data(plaintext, 1000)
+ 
+# %%
+#Dataset Generation 
+key = random.randint(1,25)
+ciphertext = shift(quote, key)
+dataset = data(quote, 1000)
+quote = quote.lower()
 
 print(dataset[:5])
